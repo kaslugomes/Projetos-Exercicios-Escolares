045360a5ecb48a0fcf4c54511f9757ed4b47c277
kaslugomes <kaslugomes@gmail.com>
2026-09-16 00:27:50 +0200

    Add files via upload

--- ex18.java
+++ ex18.java
@@ -1,3 +1,26 @@
-//18.
+//18. Faca um porgrama para aprovar emprestimos bancarios. O codigo deve ter tres informacoes:
+//Dados: Valor do emprestimo;
+// Numero de parcelas;
+// Salario do solicitante;
+import java.util.Scanner;
 public class ex18 {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Insira o seu salario: ");
+        Double salario = entrada.nextDouble();
+        System.out.print("Insira o valor do emprestimo: ");
+        Double emprestimo = entrada.nextDouble();
+        System.out.print("Insira o numero de parcelas: ");
+        Integer parcelas = entrada.nextInt();
+        Double valorParcela = emprestimo / parcelas;
+
+        if (valorParcela > (0.30 * salario)){
+            System.out.println("O emprestimo nao foi aprovado, pois o valor da parcela e de: " + valorParcela + ", e o seu salario e de: " + salario + ". O valor da parcela nao pode ultrapassar 30% do seu salario.");
+        } else if (valorParcela <= (0.30 * salario)){
+            System.out.println("O emprestimo foi aprovado, pois o valor da parcela e de: " + valorParcela + ", e o seu salario e de: " + salario + ". O valor da parcela nao ultrapassa 30% do seu salario.");
+        } else{
+            System.out.println("Entrada invalida.");
+        }
+    }
 }
